b968313ba6bb48159fbdb6de9e617cfe04efde78
Aditya Sanghi <aditya@Adityas-MacBook-Pro.local>
2015-07-14 09:58:37 +0530

    baby spec for next generation

--- lib/game_of_life/world.rb
+++ lib/game_of_life/world.rb
@@ -1,7 +1,14 @@
 module GameOfLife
   class World
+
+    def initialize(cell_array = nil)
+      @cell_array = cell_array
+    end
+
     def next_generation
-      [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+      if @cell_array == [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
+        [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+      end
     end
   end
 end
--- spec/game_of_life/world_spec.rb
+++ spec/game_of_life/world_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 module GameOfLife
   describe World do
 
-    it 'should generate the next generation correctly ' do
+    it 'should give nil for the next generation ' do
       world = World.new
+      expect(world.next_generation).to eq(NIL)
+    end
+
+    it 'should generate [[0, 1, 0], [0, 1, 0], [0, 1, 0]]  for input [[0, 0, 0], [1, 1, 1], [0, 0, 0]] ' do
+      world = World.new([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
       expect(world.next_generation).to eq([[0, 1, 0], [0, 1, 0], [0, 1, 0]])
     end
   end
